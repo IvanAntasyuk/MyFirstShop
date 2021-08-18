2353fe27a4e90a0e93a8c0fdee8abebb21c1f820
IvanAntasyuk <Pikin64@gmail.com>
2021-08-18 19:37:58 +0300

    liquibase add

---
+++ shop-database/src/main/resources/db/changelog/v-4.0/insert_default_brand_category.sql
@@ -0,0 +1,8 @@
+INSERT INTO `category` (`categoryName`) VALUES ('Телефон');
+GO
+INSERT INTO `category` (`categoryName`) VALUES ('Планшет');
+GO
+INSERT INTO `brands` (`brandName`) VALUES ('Apple');
+GO
+INSERT INTO `brands` (`brandName`) VALUES ('Samsung');
+GO
