7fdfcd21939f2faaf16c04fcbd823af68e2a2c2c
IvanAntasyuk <Pikin64@gmail.com>
2021-10-07 22:34:28 +0300

    Cloud

--- picture-service-app/src/main/java/ru/geekbrains/antasyuk/PictureServiceAppApplication.java
+++ picture-service-app/src/main/java/ru/geekbrains/antasyuk/PictureServiceAppApplication.java
@@ -1,4 +1,4 @@
-package ru.geekbrains.antasyuk.pictureserviceapp;
+package ru.geekbrains.antasyuk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PictureServiceAppApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PictureServiceAppApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(PictureServiceAppApplication.class, args);
+	}
 
 }
--- picture-service-app/src/test/java/ru/geekbrains/antasyuk/PictureServiceAppApplicationTests.java
+++ picture-service-app/src/test/java/ru/geekbrains/antasyuk/PictureServiceAppApplicationTests.java
@@ -1,4 +1,4 @@
-package ru.geekbrains.antasyuk.pictureserviceapp;
+package ru.geekbrains.antasyuk;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PictureServiceAppApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
