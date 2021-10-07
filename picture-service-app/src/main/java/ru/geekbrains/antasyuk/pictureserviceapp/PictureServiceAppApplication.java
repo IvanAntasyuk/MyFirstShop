7cd542734b1eb3340c8fc83c62a9748687f07614
IvanAntasyuk <Pikin64@gmail.com>
2021-10-07 14:52:01 +0300

    Cloud

---
+++ picture-service-app/src/main/java/ru/geekbrains/antasyuk/pictureserviceapp/PictureServiceAppApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.antasyuk.pictureserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureServiceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PictureServiceAppApplication.class, args);
+    }
+
+}
